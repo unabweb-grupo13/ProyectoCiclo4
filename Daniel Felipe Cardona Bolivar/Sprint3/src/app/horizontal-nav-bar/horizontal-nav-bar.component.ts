96d805dea3121d0f0343b4db310269a568eb6b0d
DanielSilencio <daxcardona@hotmail.com>
2021-12-08 19:42:06 -0500

    Subiendo el sprint3

---
+++ Daniel Felipe Cardona Bolivar/Sprint3/src/app/horizontal-nav-bar/horizontal-nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-horizontal-nav-bar',
+  templateUrl: './horizontal-nav-bar.component.html',
+  styleUrls: ['./horizontal-nav-bar.component.css']
+})
+export class HorizontalNavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
